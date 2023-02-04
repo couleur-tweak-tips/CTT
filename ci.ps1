da549d39b0d94a14d2749bfd5ddbaca644c43b5a
Couleur <82747632+couleurm@users.noreply.github.com>
2023-02-04 21:19:42 +0100

    refactor embed metadata, better filter

--- ci.ps1
+++ ci.ps1
@@ -9,10 +9,21 @@ if (!(Get-Module powershell-yaml)){
 
 . ./lib.ps1
 
-Get-ChildItem ./webhooks/ -Recurse -File | ForEach-Object {
+$webhooks = Get-ChildItem ./webhooks/ -Recurse -File
+# Gets an array all files within the webhooks folder
 
-    $Name = $PSItem.BaseName
-    $Content = Get-Content $PSItem -Raw | ConvertFrom-Yaml
-    $Content | Post-Webhook
-    $Content | ConvertFrom-WebHook | Set-Content ./docs/$Name`.md
+foreach ($embed in $webhooks){
+
+    $Name = $embed.BaseName
+    # e.g /webhooks/obs.yaml -> obs
+
+    Post-WebHook $embed
+
+    Get-Content $Embed -Raw | # 1. Get the content of the file
+        ConvertFrom-YAML | # 2. Deserialize it into a Hashtable object
+            ConvertFrom-WebHook | # 3. ConvertFrom-WebHook outputs a multi-line string of markdown
+                Set-Content ./docs/$Name`.md # 4. Set-Content writes what is piped to a file that has the same name
+    
+    
+   
 }
--- lib.ps1
+++ lib.ps1
@@ -1,56 +1,74 @@
 function Post-Webhook {
     param(
         [Parameter(ValueFromPipeline = $true)]
-        [Hashtable]$Payload
+        [System.IO.FileInfo]$Path
     )
 
-    $Payload | Filter-Pings
+    $Payload = Get-Content $Path | ConvertFrom-Yaml
 
-    "secret", "message_id", "channel" | ForEach-Object {
-        if ([Array]$Payload.keys -notcontains $_){
-            "Webhook needs the following keys: secret, message_link and channel"
-        }
+    $Keys = [Array]$Payload.Keys
+
+    if ('channel' -notin $Keys) {
+        Write-Warning "Webhook ``$($Path.Name)`` needs the following keys: secret, message_link and channel"
+        return
     }
-    Write-Warning ([bool])
 
-    $channel = $Payload.channel
-    $Token = (Get-Item "env:$($Payload.secret)" -ErrorAction Stop).Value
-    $message_id = $Payload.message_id
+<#
+    Sample of a (dead) webhook URL:
+    https://discord.com/api/webhooks/1071479561239396523/3AEEnB5VhR130TDaFtinu2M_vMVJZSiMwi2jLTHiM6gTXkb_scjVsKx-6gW736Q9w7dN
     
+    `channel` should just contain
+    1071479561239396523/3AEEnB5VhR130TDaFtinu2M_vMVJZSiMwi2jLTHiM6gTXkb_scjVsKx-6gW736Q9w7dN
+#>
+    # Parameters is a hashtable that is gonna be fed as a splat to Invoke-WebRequest
+    # See splatting: https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_splatting
+    # Invoke-WebRequest docs: https://go.microsoft.com/fwlink/?LinkID=2097126
     $Parameters = @{
-        Uri = "https://discord.com/api/webhooks/$channel/$token/messages/$message_id"
-        Method = "Patch"
-        Body = $Payload | ConvertTo-Json -Depth 99 -Compress
+        Uri = ("https://discord.com/api/webhooks/" + (Get-Item "env:$($Payload.channel)").Value)
+        Body = Format-Embed $Payload.clone()
         Headers = @{ "Content-Type" = "application/json" }
     }
 
-    if ($env:COULEUR){Wait-Debugger}
+
+    if ($Payload.message_link){
+        # Then we're modifying an existing message, we need to specify which via the message link
+        $Parameters.Uri += "/messages/$($Payload.message_link)"
+        $Parameters.Method = "Patch"
+    } else {
+        $Parameters.Method = "Post"
+    }
+
+    if ($Payload.thread_id){
+        # Then we need to provide context over which creation/modification this is in
+        $Parameters.Uri += "?thread_id=$($Payload.thread_id)"
+    }
+
+    # if ($env:COULEUR){ Wait-Debugger }
 
     Write-Debug ($Parameters | ConvertTo-Yaml)
 
     Invoke-WebRequest @Parameters
-    
 }
 
-function Filter-Pings { # void
+# Returns a JSON string to feed to the GitHub API
+# Strips pings (parse_mentions)
+function Format-Embed { 
     param(
-        [Parameter(ValueFromPipeline = $true)]
         [Hashtable]$Payload
     )
-    @(
-        '@everyone'
-        '@here'
-        '<@&\d{19}>' # roles
-    ) | ForEach-Object {
-        if ([Regex]::Matches($Payload, $PSItem)) {
-            throw "Ping detected, aborting!"
-            exit 1
-        }
+
+    foreach ($CTT_Keyword in ("channel", "message_link", "thread_id")) {
+        $Payload.remove($CTT_Keyword)
     }
-    if ([Regex]::Matches($Payload, "<@\d{18}>").count -gt 5){
-        throw "Pinged abve 5 users, aborting"
-        exit 1
+
+    # Blocks out any potential mentions
+    # https://birdie0.github.io/discord-webhooks-guide/structure/allowed_mentions.html
+    $Payload."allowed_mentions" = @{
+        "parse" = @()
+        "roles" = @()    
     }
+
+    return $Payload | ConvertTo-Json -Depth 5
 
 }
 
